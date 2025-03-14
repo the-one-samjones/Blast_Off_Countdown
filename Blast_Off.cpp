c4d7b47c3cf692bf80fbefebaf6780816c7e82c6
the-one-samjones <theonesamjones@gmail.com>
2025-03-14 18:23:10 -0500

    Added_Comments

--- Blast_Off.cpp
+++ Blast_Off.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int numInput;
+int numInput;           //Initializes a variable to hold the user input
 
 int main() {
-    cout << "Enter a number to count down from: ";
-    cin >> numInput;
-    cout << endl;
+    cout << "Enter a number to count down from: ";  //Prompt the user for input
+    cin >> numInput;                         //Store the input in numInput
+    cout << endl;                         //Print a new line
 
-    for (int i = numInput; i > 0; --i) {
-        cout << i;
-        cout << endl;
+    for (int i = numInput; i > 0; --i) {              //Loop from numInput down to 1
+        cout << i;              //Print the current number
+        cout << endl;       //Print a new line  
     }
-    cout << "Blast Off!" << endl;
+    cout << "Blast Off!" << endl;          //Print "Blast Off!" after the countdown 
 
     return 0;
 }
